3bba6e90fd6e0bc08674c7f49feaa32941505572
Bruno Wallison <brunowallison.fernandes@gmail.com>
2021-04-06 15:25:49 -0300

    feat: adding remove

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { exec } = require('child_process');
 const fs = require('fs').promises;
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
 const getDuration = async (link) => new Promise((resolve, reject) => {
   exec(`ffprobe -i ${link} -show_entries format=duration -v quiet -of csv="p=0"`, (error, stdout) => {
@@ -11,27 +12,25 @@ const getDuration = async (link) => new Promise((resolve, reject) => {
   });
 });
 
-const takeThumbnail = async (link) => {
+const createThumbnail = async (link, temporaryFolderPath) => {
   const videoDuration = await getDuration(link);
-  const now = Date.now();
-  const temporaryFolderPath = path.resolve(__dirname, 'tmp');
 
   const temporaryFolderExists = await new Promise((resolve) => {
     fs.access(temporaryFolderPath).then(() => resolve(true)).catch(() => resolve(false));
   });
-
   if (!temporaryFolderExists) await fs.mkdir(temporaryFolderPath);
-
   return new Promise((resolve, reject) => {
-    exec(`ffmpeg -ss ${(10 / 100) * Math.floor(videoDuration)} -i ${link} -vframes 1 ${path.resolve(temporaryFolderPath)}/${now}.png`, (error, stdout) => {
+    const filePath = `${path.resolve(temporaryFolderPath)}/${uuidv4()}.png`;
+    exec(`ffmpeg -ss ${(10 / 100) * Math.floor(videoDuration)} -i ${link} -vframes 1 ${filePath}`, (error, stdout) => {
       if (error) {
         return reject(error);
       }
-      return resolve(stdout);
+      return resolve(filePath);
     });
   });
 };
 
 (async () => {
-  console.log(await takeThumbnail('https://assets14.ign.com/videos/zencoder/2021/04/05/1920/6bafee57743996c202c6839447dd260d-3906000-1617642138.mp4'));
+  const filePath = await createThumbnail('https://assets14.ign.com/videos/zencoder/2021/04/05/1920/6bafee57743996c202c6839447dd260d-3906000-1617642138.mp4', path.resolve(__dirname, 'tmp'));
+  await fs.rm(filePath);
 })();
